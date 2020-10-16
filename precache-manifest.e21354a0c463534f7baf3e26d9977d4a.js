4f06e7dc918b5916209292454a43435244f32c2f
amdota <60938611+amdota@users.noreply.github.com>
2020-10-16 17:47:44 +0300

    Add files via upload

---
+++ precache-manifest.e21354a0c463534f7baf3e26d9977d4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92097d52adcd203067b63cacd3f247b0",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "de85decfe41c91367c7e",
+    "url": "/react-website/static/css/main.2d47f73e.chunk.css"
+  },
+  {
+    "revision": "df6cddfa8d7643e5a408",
+    "url": "/react-website/static/js/2.d5e26140.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-website/static/js/2.d5e26140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de85decfe41c91367c7e",
+    "url": "/react-website/static/js/main.c9fab798.chunk.js"
+  },
+  {
+    "revision": "fadfad2d28377605b9ac",
+    "url": "/react-website/static/js/runtime-main.caee497b.js"
+  }
+]);
